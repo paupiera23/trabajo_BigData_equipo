45ed969bd4ae4664e8da0d54645379e44157c29d
Pau Piera <90447703+paupiera23@users.noreply.github.com>
2021-11-30 19:30:50 +0100

    Add files via upload

---
+++ proba de codic.R
@@ -0,0 +1,104 @@
+
+#Habría que hacer un install.packages(" ") con cada uno si no los tuvieramos ya en nuestra biblioteca.
+library(tidyverse)
+library(klippy)  #- remotes::install_github("rlesur/klippy")
+library(knitr)
+library(readxl)
+library(reactable)
+library(ggplot2)
+library(stringi)
+library(ggThemeAssist)
+library(plotly)
+library(patchwork)
+library(ggrepel)
+library(sf)
+library(crosstalk)
+library(gganimate)
+library(hrbrthemes)
+library(sf)
+library(RColorBrewer)
+library(rio)
+
+df_paro <- rio::import("./datos/tasa_paro.xls")
+df_natalidad <- rio::import("./datos/tasa_natalidad.xls")
+df_actividad <- rio::import("./datos/tasa_actividad.xls")
+df_vida <- rio::import("./datos/esperanza_vida.xlsx")
+df_eman <- rio::import("./datos/edad_emancipacion.xlsx")
+df_actividad_edad <- rio::import("./datos/actividad_edad.xls")
+df_min_max <- rio::import("./datos/pens_min_max.xlsx")
+df_tramo <- rio::import("./datos/pensiones_tramo.xls")
+df_pens_med <- rio::import("./datos/pension_media.xls")
+df_w_medio <- rio::import("./datos/salario_medio.xlsx")
+df_smi <- rio::import("./datos/smi.xlsx")
+
+df_paro_l_csv <- rio::import("./datos/tasa_paro.csv")
+
+rio::export(df_paro_l, "./datos/tasa_paro.csv")
+
+
+str(df_paro_l_csv)
+
+df_paro_l <- df_paro %>% pivot_longer(cols = 2:20, names_to = "Periodos", values_to = "Paro")
+
+
+library(plotly)
+
+p1 <- ggplot(df_paro_l_csv, aes(Periodos, Paro)) +
+  geom_line() + geom_point()
+
+ggplotly(p1)
+
+
+
+df_natalidad_1 <- df_natalidad %>%
+  filter(Provincias == "Total Nacional")
+
+df_natalidad_1_l <- df_natalidad_1 %>%
+  pivot_longer(cols = 2:47, names_to = "Periodos", values_to = "Tasa_Natalidad")
+
+rio::export(df_natalidad_1_l, "./datos/tasa_natalidad.csv")
+
+df_natalidad_csv <- rio::import("./datos/tasa_natalidad.csv")
+
+p1 <- ggplot(df_natalidad_csv, aes(Periodos, Tasa_Natalidad)) +
+  geom_line() + 
+  geom_point()
+
+ggplotly(p1)  
+
+df_actividad_l <- df_actividad %>%
+  pivot_longer(cols = 2:20, names_to = "Periodos", values_to = "Tasa_Actividad")
+
+rio::export(df_actividad_l, "./datos/tasa_actividad.csv")
+
+df_actividad_csv <- rio::import("./datos/tasa_actividad.csv")
+
+p3 <- ggplot(df_actividad_csv, aes(Periodos, Tasa_Actividad)) +
+  geom_line() +
+  geom_point()
+
+p3
+
+ggplotly(p3)
+
+
+df_eman_l <- df_eman %>%
+  pivot_longer(cols = 2:6, names_to = "Tramo_Edad", values_to = "Porcentaje")
+
+rio::export(df_eman_l, "./datos/edad_emancipacion.csv")
+
+df_eman_csv <- rio::import("./datos/edad_emancipacion.csv")
+
+df_eman_csv_1 <- df_eman_csv %>%
+  group_by(Tramo_Edad)
+
+p4 <- ggplot(df_eman_csv, aes(Periodo, color = Tramo_Edad)) +
+  geom_bar()
+
+p4
+
+
+rio::export(df_min_max, "./datos/pens_min_max.csv")
+
+df_min_max_csv <- rio::import("./datos/pens_min_max.csv")
+
